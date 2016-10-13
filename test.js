434ccafb962f0b625a255493d031a85b6fcab518
t1st3 <contact@tiste.org>
2016-10-13 19:27:03 +0200

    test: use async/await rather than a Promise

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 import test from 'ava';
 import isMate from './';
 
-test('is-mate', t => {
-	return isMate().then(data => {
-		t.false(data);
-	});
+test('is-mate', async t => {
+	t.is(await isMate(), false);
 });
